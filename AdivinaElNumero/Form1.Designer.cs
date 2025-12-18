cf1ed6c15b662ff05f61f4695eadce31dcad90f3
jorgefelix13 <felixrasconjorgeantonio8@gmail.com>
2025-12-17 20:57:33 -0700

    feature: subo proyecto

---
+++ AdivinaElNumero/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace AdivinaElNumero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.lblVidas = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnProbar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(122, 44);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(257, 16);
+            this.lblMensaje.TabIndex = 0;
+            this.lblMensaje.Text = "\"Estoy pensando un numero entre 1 y 100\"";
+            // 
+            // lblVidas
+            // 
+            this.lblVidas.AutoSize = true;
+            this.lblVidas.Location = new System.Drawing.Point(220, 237);
+            this.lblVidas.Name = "lblVidas";
+            this.lblVidas.Size = new System.Drawing.Size(0, 16);
+            this.lblVidas.TabIndex = 1;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(186, 87);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 22);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // btnProbar
+            // 
+            this.btnProbar.Location = new System.Drawing.Point(200, 149);
+            this.btnProbar.Name = "btnProbar";
+            this.btnProbar.Size = new System.Drawing.Size(86, 40);
+            this.btnProbar.TabIndex = 3;
+            this.btnProbar.Text = "Intentar";
+            this.btnProbar.UseVisualStyleBackColor = true;
+            this.btnProbar.Click += new System.EventHandler(this.btnProbar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(294, 355);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(85, 39);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnProbar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblVidas);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Label lblVidas;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnProbar;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ AdivinaElNumero/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdivinaElNumero
+{
+    public partial class Form1 : Form
+    {
+        public int numeroSecreto;
+        public int intentos = 7;
+
+        Random rand = new Random();
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+           // numeroSecreto = rand.Next(1, 101);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Creamos un método auxiliar para no copiar y pegar lógica en el botón Limpiar
+        private void ReiniciarJuego()
+        {
+            // CORRECCIÓN 1: Ponemos 101 para que el 100 sea posible
+            numeroSecreto = rand.Next(1, 101);
+            intentos = 7;
+            txtNumero.Clear();
+            lblVidas.Text = "Te quedan intentos: " + intentos;
+            txtNumero.Focus();
+            btnProbar.Enabled = true; // Reactivamos el botón si estaba bloqueado
+        }
+
+        private void btnProbar_Click(object sender, EventArgs e)
+        {
+            int cantidadIngresada = 0;
+
+            // Validación
+            if (!int.TryParse(txtNumero.Text, out cantidadIngresada))
+            {
+                MessageBox.Show("Debe de ser numeros y no letras");
+                return;
+            }
+
+            // CORRECCIÓN 3: Usamos 'cantidadIngresada' en lugar de Convert.ToInt32
+
+            // CASO 1: GANO
+            if (cantidadIngresada == numeroSecreto)
+            {
+                MessageBox.Show("Felicidades ese es el numero del que estoy pensando");
+                btnProbar.Enabled = false; // Bloqueamos para que no siga jugando
+            }
+            // CASO 2: EL NÚMERO ES MENOR O MAYOR
+            else
+            {
+                // Restamos vida
+                intentos--;
+                lblVidas.Text = "te quedan intentos" + intentos;
+
+
+                if (cantidadIngresada > numeroSecreto)
+                {
+                    MessageBox.Show("el numero secreto es MENOR");
+                }
+                else
+                {
+                    MessageBox.Show("el numero secreto es MAYOR");
+                }
+
+                // CORRECCIÓN 2: Verificamos la derrota INMEDIATAMENTE
+                if (intentos == 0)
+                {
+                    MessageBox.Show("Game Over. El número era: " + numeroSecreto);
+                    btnProbar.Enabled = false; // Bloqueamos el botón
+                }
+            }
+            // Limpiamos la caja y ponemos foco para escribir rápido el siguiente
+            txtNumero.Clear();
+            txtNumero.Focus();
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            ReiniciarJuego();
+        }
+    }
+}
